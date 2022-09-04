e69eb4e02b295eb9ee56be98f2213b936a1fcc70
Anton <anton@mail.ru>
2022-09-05 02:46:33 +0400

    Сделана 3ая задача

--- Program.cs
+++ Program.cs
@@ -22,19 +22,17 @@
 
 //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
+System.Console.Write("Введите цифру, обозначающую день недели (от 1 до 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(1< day & day > 7){
+    System.Console.WriteLine("Введите корректный день недели: от 1 до 7");
+}
+else if(day == 6 ^ day == 7){
+        System.Console.WriteLine("Да");
+}
+else{
+        System.Console.WriteLine("Нет");
+}
 
 
-
-
-
-
-// System.Console.Write("Введите трехзначное число: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-
-// int d1 = a % 10;  // Первая цифра
-// int d2 = a % 100 / 10; // Вторая цифра
-// int d3 = a / 100 % 10; // Третья цифра
-
-// Console.WriteLine($"Первая цифра {d1}, вторая цифра {d2}, третья {d3}");
-// System.Console.WriteLine(d3*10 + d1);
-// //Console.ReadLine(a);
+;
